cc9b0fd9e52892f45304e2f86ebeb36f2ba888d3
Rittah254 <rittahjustine@yahoo.com>
2025-05-04 17:21:29 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "replace here with backend name"
+    bucket  = "replace here with backet name"
     key     = "alb/terraform.state"
     region  = "us-east-1"
     encrypt = true
